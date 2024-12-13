bddb54dbd1755ba8c8846bb24f7e23b45b250aaa
unxok <anderluke707@gmail.com>
2024-12-12 21:15:23 -0800

    removed testing example for types

--- src/libs/types/index.d.ts
+++ src/libs/types/index.d.ts
@@ -1,18 +1,1 @@
-import { Events } from "obsidian";
-
-declare module "obsidian" {
-	interface Workspace extends Events {
-		/** @internal Triggers when user right-clicks on external URL in editor */
-		on(
-			name: "file-property-menu",
-			callback: (menu: Menu, property: string) => void,
-			ctx?: unknown
-		): EventRef;
-
-		randomTest(): void;
-	}
-
-	interface AbstractInputSuggest<T> extends PopoverSuggest<T> {
-		showSuggestions: (suggestions: T[]) => void;
-	}
-}
+// use this file to add augmentations to obsidian's typings
